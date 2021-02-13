295edb51270ea7a1c2a4e162ed4226992791f310
patman007 <patrickmason007@gmail.com>
2021-02-12 23:34:55 -0600

    Project 6 and 7

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -1,4 +1,18 @@
 // select modal-btn,modal-overlay,close-btn
-// listen for click events on modal-btn and close-btn
-// when user clicks modal-btn add .open-modal to modal-overlay
-// when user clicks close-btn remove .open-modal from modal-overlay
+const modalBtn = document.querySelector('.modal-btn')
+const modalOverlay = document.querySelector('.modal-overlay')
+const closeBtn = document.querySelector('.close-btn');
+
+// listen for click event on modal-btn
+modalBtn.addEventListener('click', function() {
+    // when user clicks modal-btn add .open-modal to modal-overlay
+    modalOverlay.classList.add('open-modal');
+    console.log('checked')
+})
+
+// listen for click event on close-btn
+closeBtn.addEventListener('click', function() {
+    // when user clicks close-btn remove .open-modal from modal-overlay
+    modalOverlay.classList.remove('open-modal');
+    console.log('uncheck')
+})
--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,39 @@
-//using selectors inside the element
-// traversing the dom
+//Way 1
+
+// //Using selectors inside the element
+// const btns = document.querySelectorAll('.question-btn');
+
+// //Use forEach loop to go through all selectors of
+// //questions using question-btn
+// btns.forEach(function(btn) {
+
+//     // traversing the dom
+//     //Button Event Listener for each element in loop
+//     //that can be selected for the element(e)
+//     btn.addEventListener('click', function(e) {
+
+//         //parent Elements are selected for minus or plus classes
+//         const question = e.currentTarget.parentElement.parentElement;
+//         console.log(question);
+//         question.classList.toggle("show-text");
+//     });
+// });
+
+//////////////////////////////////////////////////////////////////////
+
+//Way 2
+//Using selectors inside the element
+const questions = document.querySelectorAll('.question')
+console.log(questions)
+
+//Use forEach loop to go through all selectors of
+//questions using question-btn
+questions.forEach(function(question) {
+    console.log(e);
+    const btn = question.querySelector('.question-btn');
+    console.log(btn);
+    btn.addEventListener('click', function() {
+        question.classList.toggle('show-text');
+    });
+});
+
