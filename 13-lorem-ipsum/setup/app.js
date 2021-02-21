547c622774f4b866c4249a4ce0079f229d2d56af
patman007 <patrickmason007@gmail.com>
2021-02-21 16:25:04 -0600

    Project 13

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -11,3 +11,46 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
   `Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.`,
   `Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.`,
 ];
+
+//Define variables
+const form = document.querySelector('.lorem-form');
+const amount = document.getElementById('amount');
+const result = document.querySelector('.lorem-text');
+
+//Form
+form.addEventListener('submit',function(e) {
+  e.preventDefault();
+  //Define variables inside form
+  const value =  amount.value;
+  const random = Math.floor(Math.random() * text.length);
+
+  //shows the term 'string'
+  //Black font will be shown in the console
+  console.log(typeof value);
+  console.log(value);
+
+  //shows the term NaN or an actual number
+  //Blue font will be shown in the console
+  console.log(parseInt(amount.value));
+
+  //empty
+  // -1
+  // > 9
+  //IF Statement using OR ||
+  if(isNaN(value) || value <= 0 || value >= 9){
+    // result.innerHTML = `<p class='result'>${text[0]}</p>`;
+    result.innerHTML = `<p class='result'>${text[random]}</p>`;
+  }
+  else {
+    //Using slice method
+    let tempText = text.slice(0, value);
+    //iterating over tempText array
+    tempText = tempText.map(function(item) {
+      return`<p class='result'>${item}</p>`;
+    })
+    .join("");
+    result.innerHTML = tempText
+    // console.log(tempText);
+  }
+});
+
